ebf844d82ce475a68270232e5e87e031819c241b
HallanBR <hallansousamartins1@gmail.com>
2025-03-21 17:55:32 -0300

    adiciona comentários explicativos sobre o método main na classe MinhaClasse

--- Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse.java
+++ Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse.java
@@ -5,3 +5,22 @@ public class MinhaClasse {
     }
 }
 
+/* public static void main(String[] args)  e a assinatura do metodo main, o ponto de entrada para qualquer aplicacao java*/
+
+    /* public: e um modificador de acesso, e este indica que o metodo pode ser acessado de qualqer lugar */
+
+    /* static: este modificador indica que o metodo pertence a classe em si, e nao a uma instancia especifica da classe.*/
+
+    /* void: Este e o tipo de retorno do metodo. Significa que o metodo nao retorna nenhum valor */
+
+    /* main: Este e o nome do metodo. A JVM procura por um metodo chamado "main" para iniciar a execucao do programa*/
+
+    /* String[] args: Este e o parametro do metodo "main". E um array de string que permite passar argumentos de linha de comando para programa */
+
+
+/* System.out.println("Hello, World!");  e o comando que imprime a mensagem Hello, World! no console. */
+
+    /* System.out: Este e um objeto que representa o fluxo de saida padrao, geralmente o console. */
+
+    /* println(): Este e um metodo do objeto "System.out" que imprime uma String no console e adiciona uma nova linha no final.*/
+
