729676a01a4d3e507f0b7386ca36ff2fc5698add
HallanBR <hallansousamartins1@gmail.com>
2025-03-23 19:00:23 -0300

    adiciona a classe MinhaClasse4 com método main para exibir mensagens baseadas na nota final

---
+++ Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse4.java
@@ -0,0 +1,27 @@
+public class MinhaClasse4 {
+    public static void main(String[] args) {
+
+        int NotaFinal = 7;
+     
+        if (NotaFinal==10) {
+            System.out.println("Ce e o bichao memo hein doido KKKK");
+            System.out.println("Sua nota e " + NotaFinal);
+        }
+
+        else if (NotaFinal>6) {
+            System.out.println("Aprovado");
+            System.out.println("Sua nota e " + NotaFinal);
+        }
+
+         else if (NotaFinal==6) {
+            System.out.println("Na risca hein malandro KKKK");
+            System.out.println("Sua nota e " + NotaFinal);    
+        }
+
+         else {
+            System.out.println("Recuperação");
+            System.out.println("Sua nota e " + NotaFinal);
+
+        }
+    }
+}
