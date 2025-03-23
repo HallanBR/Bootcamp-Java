a391cf67be4cd8439349a9c2e22645b696d7db5e
HallanBR <hallansousamartins1@gmail.com>
2025-03-23 19:07:45 -0300

    adiciona comentários explicativos ao método NomeCompleto na classe MinhaClasse3

--- Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse3.java
+++ Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse3.java
@@ -1,11 +1,11 @@
 public class MinhaClasse3 {
     public static void main(String[] args) {
-        System.out.println(NomeCompleto("Joao", "Silva"));      /*Puxando o metodo nome completo e inserindo nome neles */
-    
+        // Chama o método NomeCompleto passando "Joao" e "Silva" como argumentos e imprime o resultado
+        System.out.println(NomeCompleto("Joao", "Silva"));      
     }
+
+    // Método que recebe dois parâmetros (nome e sobrenome) e retorna a concatenação dos dois com um espaço entre eles
     public static String NomeCompleto (String nome, String sobrenome) {
         return nome + " " + sobrenome;
     }
-
-    
 }
