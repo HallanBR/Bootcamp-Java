46512bd6c7e811b1c477805c5b47b249260971ed
HallanBR <hallansousamartins1@gmail.com>
2025-03-23 18:59:43 -0300

    adiciona a classe MinhaClasse3 com método main para exibir nome completo

---
+++ Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse3.java
@@ -0,0 +1,11 @@
+public class MinhaClasse3 {
+    public static void main(String[] args) {
+        System.out.println(NomeCompleto("Joao", "Silva"));      /*Puxando o metodo nome completo e inserindo nome neles */
+    
+    }
+    public static String NomeCompleto (String nome, String sobrenome) {
+        return nome + " " + sobrenome;
+    }
+
+    
+}
