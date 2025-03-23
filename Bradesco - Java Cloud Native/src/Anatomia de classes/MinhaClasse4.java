e4b690abbdbd78fcb58ce79fe8d5baa4ca4d5d66
HallanBR <hallansousamartins1@gmail.com>
2025-03-23 19:06:33 -0300

    adiciona comentários explicativos e reorganiza a lógica de verificação da nota final na classe MinhaClasse4

--- Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse4.java
+++ Bradesco - Java Cloud Native/src/Anatomia de classes/MinhaClasse4.java
@@ -1,27 +1,30 @@
 public class MinhaClasse4 {
+
     public static void main(String[] args) {
 
+        // Declaração da variável NotaFinal e atribuição do valor 7
         int NotaFinal = 7;
      
-        if (NotaFinal==10) {
+        // Verifica se a nota final é igual a 10
+        if (NotaFinal == 10) {
             System.out.println("Ce e o bichao memo hein doido KKKK");
             System.out.println("Sua nota e " + NotaFinal);
         }
-
-        else if (NotaFinal>6) {
-            System.out.println("Aprovado");
-            System.out.println("Sua nota e " + NotaFinal);
-        }
-
-         else if (NotaFinal==6) {
-            System.out.println("Na risca hein malandro KKKK");
-            System.out.println("Sua nota e " + NotaFinal);    
-        }
-
-         else {
-            System.out.println("Recuperação");
-            System.out.println("Sua nota e " + NotaFinal);
-
-        }
+            // Verifica se a nota final é maior que 6
+            else if (NotaFinal > 6) {
+                System.out.println("Aprovado");
+                System.out.println("Sua nota e " + NotaFinal);
+            }
+                // Verifica se a nota final é igual a 6
+                else if (NotaFinal == 6) {
+                    System.out.println("Na risca hein malandro KKKK");
+                    System.out.println("Sua nota e " + NotaFinal);    
+                }
+                    // Verifica se a nota final é menor que 6
+                    else if (NotaFinal < 6) {
+                        System.out.println("Recuperação");
+                        System.out.println("Sua nota e " + NotaFinal);
+                    }
+                    
     }
 }
