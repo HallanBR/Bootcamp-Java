b07e0de00c551b99232615b591a697d54d085cc2
HallanBR <hallansousamartins1@gmail.com>
2025-03-27 21:12:41 -0300

    adiciona comentários explicativos na classe ClassificacaoCliente para melhorar a legibilidade do código

--- Bradesco - Java Cloud Native/src/Projetinhos/Classificacao de clientes/ClassificacaoCliente.java
+++ Bradesco - Java Cloud Native/src/Projetinhos/Classificacao de clientes/ClassificacaoCliente.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class ClassificacaoCliente {
     public static void main(String[] args) {
+        // Cria um objeto Scanner para capturar a entrada do usuário
         Scanner scanner = new Scanner(System.in);
 
+        // Lê o saldo do cliente fornecido pelo usuário
         double saldoCliente = scanner.nextDouble();
 
-        if (saldoCliente < 0){
+        // Verifica a classificação do cliente com base no saldo
+        if (saldoCliente < 0) {
+            // Caso o saldo seja negativo
             System.out.println("Negativado");
-        } else if(saldoCliente >=0 && saldoCliente <=500){
+        } else if (saldoCliente >= 0 && saldoCliente <= 500) {
+            // Caso o saldo esteja entre 0 e 500 (inclusive)
             System.out.println("Baixo");
-        } else if(saldoCliente > 500){
+        } else if (saldoCliente > 500) {
+            // Caso o saldo seja maior que 500
             System.out.println("Confortavel");
         }
 
+        // Fecha o objeto Scanner para liberar recursos
         scanner.close();
     }
 }
