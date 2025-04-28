14317410248b53fe94ffdf4b1957d7219f5f5ec2
HallanBR <hallansousamartins1@gmail.com>
2025-04-28 12:06:20 -0300

    adiciona loop de validação de senha com mensagens de erro e sucesso e comentarior sobre o codigo

--- Bradesco - Java Cloud Native/src/Exercicios/Exercicio1.java
+++ Bradesco - Java Cloud Native/src/Exercicios/Exercicio1.java
@@ -5,14 +5,26 @@ public class Exercicio1 {
 
     Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Digite a senha: ");
+        // Solicita ao usuario que digite a senha
+        // A senha correta é 2025
+
     int x = scanner.nextInt();
+        // Escaneia o valor digitado pelo usuario e armazena em x
 
         while (x != 2025) {        
             System.out.println("Senha Invalida");
+            System.out.println("Digite a senha novamente: ");
             x = scanner.nextInt();
         }
+        // Cria um loop que continua enquanto o valor de x for diferente de 2025
+        // se o usuario digitar um valor diferente de 2025, exibe senha invalida
+        // e solicita que o usuario digite a senha novamente
+        // Quando o usuario digita 2025, o loop termina e o programa continua
     
         System.out.println("Acesso Permitido");
+        // Exibe mensagem de acesso permitido
+
     scanner.close();
     }
     
