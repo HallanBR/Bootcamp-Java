9d1b9d18dd7a55a67acae457f82bb46c330ae31f
HallanBR <hallansousamartins1@gmail.com>
2025-05-04 20:22:25 -0300

    adiciona a classe Exercicios01 para calcular área, perímetro e diagonal de um retângulo

---
+++ Java/src/Exercicios/Ecercicios01.Readme.md
@@ -0,0 +1,2 @@
+Fazer um programa para ler os valores da largura e altura de um retangulo. 
+Em seguida, mostrar na tela o valor de sua area, perimetro e diagonal.
---
+++ Java/src/Exercicios/Exercicios01.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercicios01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double altura;
+        double largura;
+    
+
+        System.out.println("insira a altura");
+        altura = scanner.nextDouble();
+        
+        System.out.println("Insira a largura");
+        largura = scanner.nextDouble();
+
+
+        double area = largura * altura;
+
+        double perimetro = (largura + altura)*2;
+
+        double diagonal = Math.sqrt(area);
+        diagonal = Math.round(diagonal * 100.0) / 100.0;
+
+
+        System.out.println("A area e: " + area);
+        System.out.println("O perimetro e: " + perimetro);
+        System.out.println("A diagonal e: " + diagonal);
+
+
+        scanner.close();
+    }
+    
+}
