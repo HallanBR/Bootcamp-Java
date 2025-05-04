962fdc539f1f461da57ecba4e1b850e295895e15
HallanBR <hallansousamartins1@gmail.com>
2025-05-04 19:44:22 -0300

    atualiza o método main na classe Programa para adicionar e remover produtos com exibição das informações atualizadas

--- Java/src/Projetinhos/Estoque de produtos/Programa.java
+++ Java/src/Projetinhos/Estoque de produtos/Programa.java
@@ -15,9 +15,24 @@ public class Programa {
         products.price = sc.nextDouble();
         System.out.println("Insira a quantidade");
         products.quantity = sc.nextInt();
-        
+
         //* 5. Exibe as informações do produto utilizando o método toString() da classe Products.
         System.out.println(products);
+
+        System.out.println();
+
+        System.out.println("Insira a quantidade de produtos que deseja adicionar");
+        int quantity = sc.nextInt();
+        products.addProducts(quantity);
+        System.out.println();
+        System.out.println("Dados atualizados: " + products);
+
+        System.out.println("Insira a quantidade de produtos que deseja remover");
+        quantity = sc.nextInt();
+        products.removeProducts(quantity); 
+        System.out.println();
+        System.out.println("Dados atualizados: " + products);
+
 
 
 
