e00fb159b8af01dec5096e1809a7c188a54f6227
HallanBR <hallansousamartins1@gmail.com>
2025-05-26 21:22:26 -0300

    adiciona implementação da classe vetores para leitura e cálculo da média de valores em um vetor

---
+++ Java/src/Comportamento de memoria, Arrays e Listas/vetores.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class vetores {
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        // Lê o tamanho do vetor
+        System.out.println("Digite a quantidade de numeros que deseja armazenar: ");
+        int n = sc.nextInt();
+        double [] vect = new double[n];
+
+        // Lê os valores e armazena no vetor
+        for (int i=0; i<n; i++){
+            vect [i] = sc.nextDouble();
+        }
+
+
+        double soma = 0.0;
+        // Soma todos os valores do vetor
+        for (int i=0; i<n; i++){
+            soma += vect[i];
+        }
+        
+        // Calcula a média
+        double media = soma / n;
+
+        // Exibe a média
+        System.out.println("A media e : " + media);
+
+        sc.close();
+    }
+}
